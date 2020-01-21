413bb1d7be2c6aaa7c91ff3112e9d199c4100f21
Tyler Hall <rth@tyler.io>
2020-01-21 10:20:10 -0600

    Refactoring + more useful output.

--- StringsDiffer/main.swift
+++ StringsDiffer/main.swift
@@ -8,65 +8,88 @@
 
 import Foundation
 
-func keyArray(_ lines: [String]) -> Set<String> {
-    var keys = Set<String>()
-    for line in lines {
-        let tmp = line.trimmingCharacters(in: .whitespacesAndNewlines)
-        if tmp.hasPrefix("\"") {
-            let parts = tmp.components(separatedBy: "=")
-            let firstHalf = parts.first?.trimmingCharacters(in: .whitespacesAndNewlines)
-            if let key = firstHalf?.replacingOccurrences(of: "\"", with: "") {
-                keys.insert(key)
-            }
+func keyFromStringsLine(_ line: String) -> String? {
+    let tmp = line.trimmingCharacters(in: .whitespacesAndNewlines)
+    if tmp.hasPrefix("\"") {
+        let parts = tmp.components(separatedBy: "=")
+        let firstHalf = parts.first?.trimmingCharacters(in: .whitespacesAndNewlines)
+        if let key = firstHalf?.replacingOccurrences(of: "\"", with: "") {
+            return key
         }
     }
-    return keys
+    return nil
 }
 
 if CommandLine.arguments.count != 3 {
-    print("Usage: StringsDiffer TruthFile.strings Translation.strings")
+    print("Usage: StringsDiffer file1.strings file2.strings")
     exit(EXIT_FAILURE)
 }
 
-let truth = CommandLine.arguments[1]
-let trans = CommandLine.arguments[2]
+let filename1 = CommandLine.arguments[1]
+let filename2 = CommandLine.arguments[2]
 
-if !FileManager.default.fileExists(atPath: truth) {
-    print("Truth.strings does not exist.")
+if !FileManager.default.fileExists(atPath: filename1) {
+    print("\(filename1) does not exist.")
     exit(EXIT_FAILURE)
 }
 
-if !FileManager.default.fileExists(atPath: trans) {
-    print("Translation.strings does not exist.")
+if !FileManager.default.fileExists(atPath: filename2) {
+    print("\(filename2) does not exist.")
     exit(EXIT_FAILURE)
 }
 
-guard let truthStr = try? String(contentsOfFile: truth) else {
-    print("Could not read Truth.strings")
+guard let file1Contents = try? String(contentsOfFile: filename1) else {
+    print("Could not read \(filename1)")
     exit(EXIT_FAILURE)
 }
-let truthLines = truthStr.components(separatedBy: "\n")
-let truthKeys = keyArray(truthLines)
 
-guard let transStr = try? String(contentsOfFile: trans) else {
-    print("Could not read Translation.strings")
+guard let file2Contents = try? String(contentsOfFile: filename2) else {
+    print("Could not read \(filename2)")
     exit(EXIT_FAILURE)
 }
-let transLines = transStr.components(separatedBy: "\n")
-let transKeys = keyArray(transLines)
 
-var missingKeys = truthKeys.subtracting(transKeys)
-print("Translation.strings is missing \(missingKeys.count) keys")
+print("================== COMPARING =============")
+print(filename1)
+print("vs")
+print(filename2)
 print("==========================================")
-for key in missingKeys {
-    print("\(key)")
+print("\n")
+
+let file1Lines = file1Contents.components(separatedBy: "\n")
+var file1Keys = Set<String>()
+var file1Dict = [String: String]()
+for line in file1Lines {
+    if let key = keyFromStringsLine(line) {
+        file1Keys.insert(key)
+        file1Dict[key] = line
+    }
+}
+
+let file2Lines = file2Contents.components(separatedBy: "\n")
+var file2Keys = Set<String>()
+var file2Dict = [String: String]()
+for line in file2Lines {
+    if let key = keyFromStringsLine(line) {
+        file2Keys.insert(key)
+        file2Dict[key] = line
+    }
+}
+
+let file2MissingKeys = file1Keys.subtracting(file2Keys)
+let file1MissingKeys = file2Keys.subtracting(file1Keys)
+
+print("\(filename1)")
+print("MISSING \(file1MissingKeys.count) KEYS")
+print("==========================================")
+for key in file1MissingKeys {
+    print(file2Dict[key] ?? key)
 }
 
 print("\n")
 
-missingKeys = transKeys.subtracting(truthKeys)
-print("Truth.strings is missing \(missingKeys.count) keys")
+print("\(filename2)")
+print("MISSING \(file2MissingKeys.count) KEYS")
 print("==========================================")
-for key in missingKeys {
-    print("\(key)")
+for key in file2MissingKeys {
+    print(file1Dict[key] ?? key)
 }
